e765bc0aab66ae1fcfe771dc53e273c328e3546d
maria elisa <mariaelisalima04@gmail.com>
2025-01-02 14:50:33 -0300

    database and authentication

---
+++ src/user/user.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+
+    @Get('me')
+    getMe(@GetUser() user: User,){
+        console.log({
+            user: user,
+        })
+        return user;
+    }
+}
